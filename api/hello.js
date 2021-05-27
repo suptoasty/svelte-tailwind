285e4b384ec9789b28fe49405dffaa9693a192e5
suptoasty <jason.lonsinger@gmail.com>
2021-05-26 20:20:33 -0500

    faunadb test

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,31 @@
-module.exports = (req, res) => {
-	const { name = "World" } = req.query
-	res.status(200).send(`Hello ${name}!`)
+import faunadb, { query as q } from "faunadb"
+import { each } from "svelte/internal"
+
+const { FAUNADB_SECRET: secret } = process.env
+
+let client
+
+if (secret) {
+	client = new faunadb.Client({ secret })
+}
+
+export default async (req, res) => {
+	try {
+		let collections = []
+
+		if (!client) {
+			return res
+				.status(500)
+				.json({ error: new Error("Missing secret to connect to FaunaDB") })
+		}
+
+		await client.paginate(q.Collections()).map((ref) => q.Get(ref))
+		each((page) => {
+			collections = collections.concat(page)
+		})
+
+		res.json({ collections })
+	} catch (error) {
+		res.status(500).json({ error })
+	}
 }
