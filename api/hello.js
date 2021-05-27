0698034dc713b7a19b58bde5aedc39265594a5ed
suptoasty <jason.lonsinger@gmail.com>
2021-05-26 20:44:10 -0500

    faunadb test

--- api/hello.js
+++ api/hello.js
@@ -14,6 +14,7 @@ export default async (req, res) => {
 		let collections = []
 
 		if (!client) {
+			console.log(client)
 			return res
 				.status(500)
 				.json({ error: new Error("Missing secret to connect to FaunaDB") })
